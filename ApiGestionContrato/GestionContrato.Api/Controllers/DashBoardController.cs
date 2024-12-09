af3c01278206b589db05fe44afa341e8f7a05d68
OmarSC <serquencoronadoomardavid@gmail.com>
2024-12-09 17:41:59 -0500

    dashboard

---
+++ ApiGestionContrato/GestionContrato.Api/Controllers/DashBoardController.cs
@@ -0,0 +1,77 @@
+﻿using GestionContrato.BLL.Services;
+using GestionContrato.BLL.Services.Interfaces;
+using GestionContrato.DAL.Repositorios;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GestionContrato.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DashBoardController : ControllerBase
+    {
+        private readonly IDashboardService service;
+
+        public DashBoardController(IDashboardService _service)
+        {
+            service = _service;
+        }
+
+        [HttpGet]
+        [Route("por-estado")]
+        public async Task<FG<object>> contratosPorEstado()
+        {
+            var response = new FG<object>(false, new { }, "");
+            try
+            {
+                var data = await service.contratosPorEstado();
+                response = new FG<object>(true, data, "Contratos por estado obtentidos con éxito");
+                return response;
+
+            }
+            catch (Exception ex)
+            {
+                response = new FG<object>($"{ex.Message}");
+                return response;
+            }
+        }
+
+        [HttpGet]
+        [Route("por-proveedor")]
+        public async Task<FG<object>> contratosPorProveedor()
+        {
+            var response = new FG<object>(false, new { }, "");
+            try
+            {
+                var data = await service.contratosPorProveedor();
+                response = new FG<object>(true, data, "Contratos por Proveedor obtentidos con éxito");
+                return response;
+
+            }
+            catch (Exception ex)
+            {
+                response = new FG<object>($"{ex.Message}");
+                return response;
+            }
+        }
+
+        [HttpGet]
+        [Route("por-tipo")]
+        public async Task<FG<object>> contratosPorTipo()
+        {
+            var response = new FG<object>(false, new { }, "");
+            try
+            {
+                var data = await service.contratosPorTipo();
+                response = new FG<object>(true, data, "Contratos por Tipo obtentidos con éxito");
+                return response;
+
+            }
+            catch (Exception ex)
+            {
+                response = new FG<object>($"{ex.Message}");
+                return response;
+            }
+        }
+    }
+}
--- ApiGestionContrato/GestionContrato.BLL/Services/Interfaces/IDashboardService.cs
+++ ApiGestionContrato/GestionContrato.BLL/Services/Interfaces/IDashboardService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using GestionContrato.Dto;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,8 @@ namespace GestionContrato.BLL.Services.Interfaces
 {
     public interface IDashboardService
     {
+        Task<List<DashEstadoDto>> contratosPorEstado();
+        Task<List<DashTipoContratoDto>> contratosPorTipo();
+        Task<List<DashPorProveedorDto>> contratosPorProveedor();
     }
 }
